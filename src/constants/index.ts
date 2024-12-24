f1e927ed1ad9a3945af6272881a91abc6787e791
Lance Guyatt <lanceguyatt@gmail.com>
2024-12-24 13:37:09 +0000

    url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,11 @@
 import { z } from 'astro:content'
 
+const BASE_URL = import.meta.env.PUBLIC_BASE_URL
+
 export const siteSchema = z.object({
   name: z.string(),
   description: z.string(),
+  url: z.string(),
   image: z.object({
     secure_url: z.string(),
   }),
@@ -11,7 +14,8 @@ export const siteSchema = z.object({
 export const site = siteSchema.parse({
   name: 'Diz Toba',
   description: 'Diz Toba website',
+  url: BASE_URL,
   image: {
-    secure_url: 'https://diztoba.com/images/logo.png',
+    secure_url: 'https://diztoba.com/images/logo.jpg',
   },
 })
