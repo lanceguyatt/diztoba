9606e4fdec8d8d81eca7180b88a7abf44f2bbff9
Lance Guyatt <lanceguyatt@gmail.com>
2024-12-31 06:26:28 +0000

    css

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 declare module '@alpinejs/focus'
 declare module '@alpinejs/persist'
 declare module '@alpinejs/ui'
+declare module '@vimeo/player'
 
 // interface ImportMetaEnv {
 //   readonly PUBLIC_BASE_URL: string
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
-}
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
